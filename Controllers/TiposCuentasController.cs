a7280a3e8660f527437be6980fcf4747742a7e9f
Kelvin Angustia <felixkelvin00@gmail.com>
2022-07-15 12:28:55 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/TiposCuentasController.cs
@@ -0,0 +1,21 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController:Controller
+    {
+        [HttpGet("api/get")]
+        public IActionResult Crear()
+        {
+            return View("kelvin");
+        }
+
+        [HttpPost("PRI")]
+        public IActionResult Crear(TiposCuentas tiposCuentas)
+        {
+            return View();
+        }
+    }
+
+}
